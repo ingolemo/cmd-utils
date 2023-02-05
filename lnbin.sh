4d817cfa12909aa8ad43db92446b0d225b249223
Adrian Room <ingolemo@gmail.com>
2023-02-05 10:02:56 +0000

    lnbin: refactor extension removal

--- lnbin.sh
+++ lnbin.sh
@@ -6,7 +6,7 @@ case "$1" in
 Usage: lnbin <target>
 
 Creates a symlink to a file in the user's bin directory
-(~/.local/bin). The symlink will not have a file extension if
+(~/.local/bin). `lnbin` will strip the file extension if
 it matches a common executable extension.
 EOF
 	exit 0
@@ -16,9 +16,10 @@ esac
 bindir="$HOME/.local/bin"
 target="$1"
 name="$(basename $target | tr '_' '-')"
+extensions=".sh .py .bash .fish .pl .rb .awk"
 
-for suff in .sh .py .bash .fish .pl .rb .awk; do
-    name="${name/$suff/}"
+for suff in $extensions; do
+    name="${name%$suff}"
 done
 
 exec rellink "$target" "$bindir/$name"
