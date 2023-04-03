97364c374ef7726c195c6ffad4ad395b732202e8
Adrian Room <ingolemo@gmail.com>
2023-04-03 12:05:10 +0100

    next: reformat

--- next.sh
+++ next.sh
@@ -7,11 +7,12 @@ Usage: next <target> <args>...
 
 Executes the next instance of a command with a certain name found in
 the path. This is useful when you want to have a script shadow a system
-utility, but also be able to call that system utility. For example, if
-an ls command exists in both /usr/bin and /usr/local/bin then \`next
-/usr/bin/ls args\` will execute \`/usr/local/bin/ls args\`. Put \`exec next
-"\$0" --color=auto "\$@"\` in a file called \`ls\` somewhere high on your
-path and all invocations of \`ls\` will have color.
+utility, but also be able to call that system utility.
+
+For example, if an ls command exists in both /usr/bin and /usr/local/bin
+then \`next /usr/bin/ls args\` will execute \`/usr/local/bin/ls
+args\`. Put \`exec next "\$0" --color=auto "\$@"\` in a file called \`ls\`
+somewhere high on your path and all invocations of \`ls\` will have color.
 EOF
 	exit 1
 	;;
@@ -26,12 +27,12 @@ set -f
 IFS=:
 
 for d in $PATH; do
-if [ "$d" = "$dir" ]; then
-	flag=1
-elif [ -n "$flag" ] && [ -x "$d/$bin" ]; then
-	exec "$d/$bin" "$@"
-fi
+	if [ "$d" = "$dir" ]; then
+		flag=1
+	elif [ -n "$flag" -a -x "$d/$bin" ]; then
+		exec "$d/$bin" "$@"
+	fi
 done
 
 printf "%s: no '%s' found beyond '%s' in \$PATH\n" "${0##*/}" "$bin" "$dir" >&2
-exit 127 
+exit 127
